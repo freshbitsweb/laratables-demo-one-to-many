3fd30c4dbcd3aa3b01af0e4a601c936ffcdb13ff
Rajan <rajan@freshbits.in>
2019-11-23 18:24:54 +0530

    customize date format of start_date column
    Signed-off-by: Rajan <rajan@freshbits.in>

--- app/User.php
+++ app/User.php
@@ -44,4 +44,13 @@ class User extends Authenticatable
             $query->where('content', 'like', "%". $searchValue ."%");
         });
     }
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'start_date',
+    ];
 }
